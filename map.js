26c3c1a8315dae8f6f470e49429706c2e90fa97c
Gergana <gery.zlatev@gmail.com>
2024-06-02 14:12:45 +0300

    bugfixes

--- map.js
+++ map.js
@@ -9,6 +9,7 @@ require([
   "esri/layers/RouteLayer",
   "esri/widgets/ScaleBar",
   "esri/widgets/Search",
+],
 
   function (
     esriConfig,
@@ -25,6 +26,7 @@ require([
     esriConfig.apiKey = "AAPKc0d231b3177f4c7aa4f8fd4cc33d408eFr2xfZB6UcOjXi_RSt5Wf0akFMAjpgs38v03BbLMmZ6YAhkj3X7qwjsqhe6tv9sN";
 
     const routeLayer = new RouteLayer();
+
     const webmap = new WebMap({
       portalItem: {
         id: "232b4d297d054b2a831a3ce629ac8495"
@@ -43,86 +45,93 @@ require([
       apiKey: esriConfig.apiKey,
       view
 
-    })
+    });
 
-    document.getElementById('search-button').addEventListener('click', function() {
-      const query = document.getElementById('search-input').value;
-      search(query);
-  });
-  
-  function search(query) {
-      const resultsContainer = document.getElementById('results');
-      resultsContainer.innerHTML = '';
-  
-      if (query.trim() === '') {
-          resultsContainer.innerHTML = '<p>Please enter a search term.</p>';
-          return;
-      }
+    // function search(query) {
+    //     const resultsContainer = document.getElementById('results');
+    //     resultsContainer.innerHTML = '';
 
-      const results = [
-          'Search result 1 for ' + query,
-          'Search result 2 for ' + query,
-          'Search result 3 for ' + query,
-      ];
-  
-      if (results.length === 0) {
-          resultsContainer.innerHTML = '<p>No results found.</p>';
-      } else {
-          const ul = document.createElement('ul');
-          results.forEach(result => {
-              const li = document.createElement('li');
-              li.textContent = result;
-              ul.appendChild(li);
-          });
-          resultsContainer.appendChild(ul);
-      }
-  }
+    //     if (query.trim() === '') {
+    //         resultsContainer.innerHTML = '<p>Please enter a search term.</p>';
+    //         return;
+    //     }
+
+    //     const results = [
+    //         'Search result 1 for ' + query,
+    //         'Search result 2 for ' + query,
+    //         'Search result 3 for ' + query,
+    //     ];
+
+    //     if (results.length === 0) {
+    //         resultsContainer.innerHTML = '<p>No results found.</p>';
+    //     } else {
+    //         const ul = document.createElement('ul');
+    //         results.forEach(result => {
+    //             const li = document.createElement('li');
+    //             li.textContent = result;
+    //             ul.appendChild(li);
+    //         });
+    //         resultsContainer.appendChild(ul);
+    //     }
+    // }
 
     const scaleBar = new ScaleBar({
       view
     });
-    view.ui.add(scaleBar, { position: "bottom-right"
+    view.ui.add(scaleBar, {
+      position: "bottom-right"
     });
 
 
 
-const homeBtn = new Home({
-  view
-});
+    const homeBtn = new Home({
+      view
+    });
 
-view.ui.add(homeBtn, {position: "top-left"});
+    view.ui.add(homeBtn, { position: "top-left" });
 
-const layerlist = new LayerList({
-  view,
-});
-view.ui.add("layer-list-btn", "top-right");
-view.ui.add(layerlist, "top-right");
-view.ui.add("basemap-gallery-btn", "top-right")
+    const layerlist = new LayerList({
+      view,
+    });
 
-const basemapGalery = new BasemapGallery({
-  view
-});
+    view.ui.add("layer-list-btn", "top-right");
+    view.ui.add(layerlist, "top-right");
 
-view.ui.add(basemapGalery, "top-right");
+    const basemapGalery = new BasemapGallery({
+      view
+    });
 
-document.getElementById("layer-list-btn").addEventListener("click", function () {
-  toggleButton("layer-list");
-});
+    view.ui.add("basemap-gallery-btn", "top-right");
+    view.ui.add(basemapGalery, "top-right");
 
-document.getElementById("basemap-gallery-btn").addEventListener("click", function () {
-  toggleButton("gallery");
-});
+    //TODO: Create Search Widget HERE!
 
-function toggleButton(element) {
-  if (element == "layer-list") {
-    const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
-    const currentProp = layerListEl.style.getPropertyValue("display");
-    layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
-  } else if (element == "gallery") {
-    const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
-    const currentPropGallery = galleryEl.style.getPropertyValue("display");
-    galleryEl.style.setProperty("display", currentPropGallery == "none" ? "block" : "none");
-  }
-}
+    view.ui.add("search-button", "bottom-left");
 
-}]);
+    document.getElementById("layer-list-btn").addEventListener("click", function () {
+      toggleButton("layer-list");
+    });
+
+    document.getElementById("basemap-gallery-btn").addEventListener("click", function () {
+      toggleButton("gallery");
+    });
+
+    document.getElementById("search-button").addEventListener('click', function () {
+      // const query = document.getElementById('search-input').value;
+      // search(query);
+      toggleButton("search");
+    });
+
+    function toggleButton(element) {
+      if (element == "layer-list") {
+        const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+        const currentProp = layerListEl.style.getPropertyValue("display");
+        layerListEl.style.setProperty("display", currentProp == "none" ? "block" : "none");
+      } else if (element == "gallery") {
+        const galleryEl = document.getElementsByClassName("esri-basemap-gallery")[0];
+        const currentPropGallery = galleryEl.style.getPropertyValue("display");
+        galleryEl.style.setProperty("display", currentPropGallery == "none" ? "block" : "none");
+      }
+    }
+
+  });
